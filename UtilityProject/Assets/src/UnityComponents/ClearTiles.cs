87f8e16014aa1818ae9f298e9d96ce537fa79b3c
Tuozi <jacques.cavaillac@gmail.com>
2023-01-07 14:27:30 +0100

    clear tiles component

---
+++ UtilityProject/Assets/src/UnityComponents/ClearTiles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Tools
+{
+    [ExecuteInEditMode]
+    public class ClearTiles : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<Tilemap>().ClearAllTiles();
+            DestroyImmediate(this);
+        }
+    }
+}
